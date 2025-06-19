7e7c5f3a57df75df9d9ed6d56955a1d3dceb7a2f
andrit <arnewmediatech@gmail.com>
2025-06-18 23:47:17 -0400

    update homepage

---
+++ src/pages/form.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import '../styles/index.css';
+
+function Form() {
+  const [date, setDate] = useState(null);
+  useEffect(() => {
+    async function getDate() {
+      const res = await fetch('/api/date');
+      const newDate = await res.text();
+      setDate(newDate);
+    }
+    getDate();
+  }, []);
+  return (
+    <main>
+      <Helmet>
+        <title>Gatsby + Node.js (TypeScript) API</title>
+      </Helmet>
+      <h1>Hi, my name is Andrew Ritter</h1>
+      <h2>I am a creative technologist
+      </h2>
+      <p>For work: I am a fullstack web developer focusing on javascript technologies like React, redux, Express, & NextJS
+      </p>
+      <p>I also dabble in photography, film, creative writing & complexity.</p>
+     
+      <br />
+      <h3>The future of the web and its interfacing is also very interesting to me.</h3>
+      <p>One of the projects I am working on in this regard is to learn Rust while programming a small collection of Raspberry Pi's to act as my home "Hyperspace".</p>
+    </main>
+  );
+}
+
+export default Form;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+// Gatsby + Node.js (TypeScript) API
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/index.css';
@@ -15,18 +16,32 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Andrew's website</title>
       </Helmet>
-      <h1>Hi, my name is Andrew Ritter</h1>
-      <h2>I am a creative technologist
-      </h2>
-      <p>For work: I am a fullstack web developer focusing on javascript technologies like React, redux, Express, & NextJS
-      </p>
-      <p>I also dabble in photography, film, creative writing & complexity.</p>
+      <header>
+        <h1 className="greeting">Hi, my name is Andrew Ritter</h1>
+      </header>
+     
+      
+      <section className="introduction">
+        <h2>I am a creative technologist
+        </h2>
+        <p>For work: I am a fullstack web developer focusing on javascript technologies like React, redux, Express, & NextJS
+        </p>
+        <p>I also dabble in photography, film, creative writing & complexity.</p>
+      </section>
+      
      
       <br />
+      <section className="details">
       <h3>The future of the web and its interfacing is also very interesting to me.</h3>
       <p>One of the projects I am working on in this regard is to learn Rust while programming a small collection of Raspberry Pi's to act as my home "Hyperspace".</p>
+      </section>
+
+      <section className="furthermore">
+        <h4>Writing, reading and arithmetic</h4>
+      </section>
+     
     </main>
   );
 }
